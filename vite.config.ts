99672752c14b1f52b0f9917d7f6a348605c9c003
Adalbertus Chris <adalbertus.chris@gmail.com>
2024-02-26 10:02:29 +0100

    Config tests (vitest)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "SWAPP_");
-  return {
-    define: {
-      plugins: [react()],
-      "process.env.SWAPP_REACT_APP_API_URL": JSON.stringify(
-        env.SWAPP_REACT_APP_API_URL
-      ),
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
 });
