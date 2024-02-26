5fc4b075708490bf78ecb19daaaeb114926653be
Adalbertus Chris <adalbertus.chris@gmail.com>
2024-02-26 15:46:45 +0100

    Fix mapCardResponses

--- src/features/cards/base/services/card-mapper.ts
+++ src/features/cards/base/services/card-mapper.ts
@@ -6,6 +6,10 @@ export const mapCardResponses = (
   compare: ([card1, card2]: [Card, Card]) => number,
   map: (model: Record<string, unknown>) => Card
 ): CardPlayers => {
+  if (!data || !data[0] || !data[1]) {
+    return null;
+  }
+
   const [card1, card2] = [map(data[0]), map(data[1])];
   const winner = compare([card1, card2]);
   const isPlayer1Winner = [0, 1].includes(winner);
