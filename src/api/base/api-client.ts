5910e0b2f8ef59961055d19df35f7feaf91c5835
Adalbertus Chris <adalbertus.chris@gmail.com>
2024-02-26 10:10:46 +0100

    Fix env variables

--- src/api/base/api-client.ts
+++ src/api/base/api-client.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const baseUrl = process.env.SWAPP_REACT_APP_API_URL;
+export const baseUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
 export const queryClient = new QueryClient();
